9c2003650efdf19696506bd5cf765c402b759eff
NtonBala <balashov.anton.1983@gmail.com>
2017-07-26 14:32:19 +0300

    lesson5 hw updated

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-var express = require('express');
-var app = express();
-
-var cors = require('cors');
-
-var blogItems = require('./data').blogItems;
+var express = require('express'),
+    app = express(),
+    cors = require('cors'),
+    blogItems = require('./data').blogItems,
+    _ = require('lodash');
 
 app.use(cors());
 
-app.get('/', function (req, res) {
-  res.json(blogItems);
+var port = process.env.PORT || 3001;
+
+var router = express.Router();
+
+router.route('/posts')
+    .get(function (req, res) {
+        res.json(blogItems);
 });
 
-app.listen(3001, function () {
-  console.log('Server listening on port 3001');
+router.route('/posts/:post_id')
+    .get(function (req, res) {
+        res.json(_.find(
+            blogItems,
+            {_id: req.params.post_id})
+        );
+    });
+
+router.route('/metainfo/:post_id')
+    .get(function (req, res) {
+        const blogItem = _.find(blogItems, {_id: req.params.post_id});
+        blogItem.metaInfo.likes = ++blogItem.metaInfo.likes;
+        res.json(blogItem.metaInfo);
+    });
+
+app.use('/', router);
+
+app.listen(port, function () {
+    console.log('Server listening on port', port);
 });
