65db76a9867d107ebfb47a19f2dc78ea0ae1a0ed
NtonBala <balashov.anton.1983@gmail.com>
2017-11-14 22:23:38 +0200

    Added body-parser middleware, added storing data in json file & reading/writing data from/to it. Added processing PUT request when incrementing post likes count.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Blog-Server
 Simple server for React-Blog returning JSON with blog items list.
+
+Cors, express router, body-parser, rest api, lodash/immitability-helper, fs. Postman.
--- index.js
+++ index.js
@@ -2,32 +2,75 @@ var express = require('express'),
     app = express(),
     cors = require('cors'),
     blogItems = require('./data').blogItems,
-    _ = require('lodash');
+    _ = require('lodash'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    update = require('immutability-helper');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 3001;
-
 var router = express.Router();
 
+//ROUTES FOR API
+//POSTS PAGE
 router.route('/posts')
     .get(function (req, res) {
-        res.json(blogItems);
-});
-
-router.route('/posts/:post_id')
-    .get(function (req, res) {
-        res.json(_.find(
-            blogItems,
-            {_id: req.params.post_id})
-        );
+        fs.readFile('./data.json', 'utf-8', function (err, data) {
+            if (err) {
+                res.send(err);
+            } else {
+                var items = JSON.parse(data);
+                res.json(items);
+            }
+        });
     });
 
-router.route('/metainfo/:post_id')
+//POST PAGE
+router.route('/posts/:post_id')
     .get(function (req, res) {
-        const blogItem = _.find(blogItems, {_id: req.params.post_id});
-        blogItem.metaInfo.likes = ++blogItem.metaInfo.likes;
-        res.json(blogItem.metaInfo);
+        fs.readFile('./data.json', 'utf-8', function (err, data) {
+            if (err) {
+                res.send(err);
+            } else {
+                var items = JSON.parse(data),
+                    id = req.params.post_id;
+
+                res.json(_.find(items, {_id: id}));
+            }
+        });
+    })
+    .put(function (req, res) {
+        fs.readFile('./data.json', 'utf-8', function (err, data) {
+            if (err) {
+                res.send(err);
+            } else {
+                var items = JSON.parse(data),
+                    id = req.params.post_id;
+
+                var newItems = _.map(items, function (item) {
+                    if (item._id === id) {
+                        return update(item, {
+                            metaInfo: {
+                                likes: {$apply: function (count) {return count + 1}}
+                            }
+                        });
+                    } else {
+                        return item;
+                    }
+                });
+
+                fs.writeFile('data.json', JSON.stringify(newItems), function (err) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.json({message: 'Likes incremented'});
+                    }
+                });
+            }
+        });
     });
 
 app.use('/', router);
