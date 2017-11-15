f007cffe14590da6e119e6d9a2ecb8fc554a82ea
NtonBala <balashov.anton.1983@gmail.com>
2017-11-15 15:10:57 +0200

    Got rid of data.js. Added comments.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Blog-Server
-Simple server for React-Blog returning JSON with blog items list.
+Simple server for React-Blog returning JSON.
 
 Cors, express router, body-parser, rest api, lodash/immitability-helper, fs. Postman.
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express'),
     app = express(),
     cors = require('cors'),
-    blogItems = require('./data').blogItems,
     _ = require('lodash'),
     bodyParser = require('body-parser'),
     fs = require('fs'),
     update = require('immutability-helper');
 
+//MIDDLEWARES
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -14,9 +14,10 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 3001;
 var router = express.Router();
 
-//ROUTES FOR API
-//POSTS PAGE
+//API ROUTES
+//POSTS ROUTE
 router.route('/posts')
+    //retrieving posts for Posts page & piechart
     .get(function (req, res) {
         fs.readFile('./data.json', 'utf-8', function (err, data) {
             if (err) {
@@ -28,8 +29,9 @@ router.route('/posts')
         });
     });
 
-//POST PAGE
+//POST ROUTE
 router.route('/posts/:post_id')
+    //retrieving data for Post page
     .get(function (req, res) {
         fs.readFile('./data.json', 'utf-8', function (err, data) {
             if (err) {
@@ -42,6 +44,7 @@ router.route('/posts/:post_id')
             }
         });
     })
+    //incrementing post likes count
     .put(function (req, res) {
         fs.readFile('./data.json', 'utf-8', function (err, data) {
             if (err) {
@@ -66,7 +69,7 @@ router.route('/posts/:post_id')
                     if (err) {
                         res.send(err);
                     } else {
-                        res.json({message: 'Likes incremented'});
+                        res.json({message: 'Post likes incremented'});
                     }
                 });
             }
